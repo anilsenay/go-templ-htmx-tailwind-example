211a93b6488955ff897c9040a14ed4f03189990b
Anıl Şenay <anil.senay@cimri.com>
2023-12-31 19:22:17 +0300

    Add loading indicator

--- view/pages/index_templ.go
+++ view/pages/index_templ.go
@@ -56,7 +56,35 @@ func Index(props PageProps) templ.Component {
 					return templ_7745c5c3_Err
 				}
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><form hx-post=\"/todos\" hx-target=\"#todos\" hx-swap=\"beforeend\" hx-on::after-request=\"if(event.detail.successful) this.reset()\" class=\"flex items-center w-full h-8 mt-2 text-sm font-medium rounded\"><button id=\"post-button\" type=\"submit\" class=\"flex items-center px-2 h-8\"><svg class=\"w-5 h-5 text-gray-400 fill-current\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M12 6v6m0 0v6m0-6h6m-6 0H6\"></path></svg></button> <input id=\"todo-input\" class=\"flex-grow h-8 ml-2.5 p-0 bg-transparent placeholder:opacity-55 text-sm border-none outline-none focus:ring-0 focus:outline-none\" name=\"todo-text\" type=\"text\" placeholder=\"add a new task\"></form></div></div></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><form hx-post=\"/todos\" hx-target=\"#todos\" hx-swap=\"beforeend\" hx-on::after-request=\"if(event.detail.successful) { this.reset() }\" hx-disabled-elt=\"#post-button\" hx-indicator=\"#spinner\" class=\"flex items-center w-full h-8 mt-2 text-sm font-medium rounded\"><style type=\"text/css\">")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Var4 := `
+                .my-indicator{
+                    display: none;
+                }
+                .my-indicator + svg{
+                    display: initial;
+                }
+                .htmx-request .my-indicator{
+                    display: initial;
+                }
+                .htmx-request .my-indicator + svg{
+                    display: none;
+                }
+                .htmx-request.my-indicator{
+                    display: initial;
+                }
+                .htmx-request.my-indicator + svg{
+                    display: none;
+                }
+            `
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var4)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</style><button id=\"post-button\" type=\"submit\" class=\"flex items-center px-2 h-8\"><svg id=\"spinner\" class=\"animate-spin h-5 w-5 text-grey my-indicator\" viewBox=\"0 0 24 24\"><path class=\"opacity-75\" fill=\"currentColor\" d=\"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z\"></path></svg> <svg class=\"w-5 h-5 text-gray-400 fill-current\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M12 6v6m0 0v6m0-6h6m-6 0H6\"></path></svg></button> <input id=\"todo-input\" class=\"flex-grow h-8 ml-2.5 p-0 bg-transparent placeholder:opacity-55 text-sm border-none outline-none focus:ring-0 focus:outline-none\" name=\"todo-text\" type=\"text\" placeholder=\"add a new task\"></form></div></div></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
