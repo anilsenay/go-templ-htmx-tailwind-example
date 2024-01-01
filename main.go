3e062c30be25113d363bb886c9e0d94f9ccff0cb
Anıl Şenay <anil.senay@cimri.com>
2024-01-01 21:41:09 +0300

    Add 404 page

--- main.go
+++ main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"github.com/anilsenay/go-htmx-example/handler"
 	"github.com/anilsenay/go-htmx-example/repository"
+	"github.com/anilsenay/go-htmx-example/view/pages"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{})
 
+	// static files
 	app.Static("/", "./public")
 
 	todoRepository := repository.NewTodoRepository()
 	todoHandler := handler.NewTodoHandler(todoRepository)
+
+	// todo routes
 	app.Get("/", todoHandler.HandleTodoPage)
 	app.Put("/todos/:id/done", todoHandler.HandleUpdateDone)
 	app.Post("/todos", todoHandler.HandlePostTodo)
+
+	// 404
+	app.Get("*", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+		c.Status(fiber.StatusNotFound)
+		return pages.NotFound().Render(c.UserContext(), c.Response().BodyWriter())
+	})
 
 	_ = app.Listen(":8080")
 }
