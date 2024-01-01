c0ae4cb694527b5fada2b13828c6158d9ceeeddc
Anıl Şenay <anil.senay@cimri.com>
2024-01-01 17:41:48 +0300

    Fix typo

--- view/pages/index_templ.go
+++ view/pages/index_templ.go
@@ -41,7 +41,7 @@ func Index(props PageProps) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Var3 := `My Todos`
+			templ_7745c5c3_Var3 := `My Todo`
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
